d52db45fb66c76dad0e7466f65591ea61961c209
Cody Baker <cody.c.baker.phd@gmail.com>
2024-05-05 19:40:06 -0400

    added pre-commit

--- src/pynwb/ndx_microscopy/testing/__init__.py
+++ src/pynwb/ndx_microscopy/testing/__init__.py
@@ -1,5 +1,21 @@
-from ._mock import MockRecordingInterface
+from ._mock import (
+    mock_LightSource,
+    mock_Microscope,
+    mock_MicroscopyOpticalChannel,
+    mock_PlanarImagingSpace,
+    mock_PlanarMicroscopySeries,
+    mock_VariableDepthMicroscopySeries,
+    mock_VolumetricImagingSpace,
+    mock_VolumetricMicroscopySeries,
+)
 
 __all__ = [
-    "MockRecordingInterface",
+    "mock_Microscope",
+    "mock_LightSource",
+    "mock_MicroscopyOpticalChannel",
+    "mock_PlanarImagingSpace",
+    "mock_VolumetricImagingSpace",
+    "mock_PlanarMicroscopySeries",
+    "mock_VariableDepthMicroscopySeries",
+    "mock_VolumetricMicroscopySeries",
 ]
