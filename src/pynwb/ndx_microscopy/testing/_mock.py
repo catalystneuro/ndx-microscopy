c4e8f3a6deee569ed2b0d94217751c865f3ed1f2
codycbakerphd <cbaker9@nd.edu>
2024-07-17 15:30:22 -0400

    fix import

--- src/pynwb/ndx_microscopy/testing/_mock.py
+++ src/pynwb/ndx_microscopy/testing/_mock.py
@@ -119,7 +119,7 @@ def mock_VolumetricImagingSpace(
 def mock_MicroscopySegmentations(
     name: Optional[str] = None,
     microscopy_plane_segmentations: Optional[Iterable[ndx_microscopy.MicroscopyPlaneSegmentation]] = None,
-) -> ndx_microscopy.MicroscopyImageSegmentation:
+) -> ndx_microscopy.MicroscopySegmentations:
     name = name or name_generator("MicroscopySegmentations")
     microscopy_plane_segmentations = microscopy_plane_segmentations or [mock_MicroscopyPlaneSegmentation()]
 
