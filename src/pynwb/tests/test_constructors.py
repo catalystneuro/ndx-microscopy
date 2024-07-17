70fa119404a781721f4faab4b467f3648121e6d6
codycbakerphd <cbaker9@nd.edu>
2024-07-17 15:48:52 -0400

    debugs

--- src/pynwb/tests/test_constructors.py
+++ src/pynwb/tests/test_constructors.py
@@ -129,7 +129,7 @@ def test_constructor_multi_channel_microscopy_volume():
 
 def test_constructor_variable_depth_multi_channel_microscopy_volume():
     microscope = mock_Microscope()
-    light_source = mock_LightSource()
+    light_source = mock_MicroscopyLightSource()
     imaging_space = mock_VolumetricImagingSpace(microscope=microscope)
     optical_channel = mock_MicroscopyOpticalChannel()
 
